545978cdcae2b18159055085baff60708ad41b55
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2022-07-24 09:32:29 -0700

    change add_table, allow removal of existing table
    Previously add_table would error out if you added a table with the same name as an existing table.
    Now there is a "force" argument (default False) that allows you to choose whether to raise an error
    or replace that table when this happens.

--- add_table.py
+++ add_table.py
@@ -1,15 +1,34 @@
 from openpyxl.worksheet.table import Table, TableStyleInfo
+from excel_ranges import range_containing_table
 import openpyxl
 
-def add_table(fname, sheet_name, tbl_name, rng):
+
+def add_table(fname, sheet_name, tbl_name, rng, force=False):
+    '''For an Excel workbook at path fname, add a table named tbl_name to 
+    range rng of sheet sheet_name.
+rng should be an Excel range of the form 'A1:B20', or a pandas DataFrame.
+    If rng is a pandas DataFrame, the table will automatically have its upper
+    left corner at cell A1 of the sheet.
+If you want to customize the location of a table, use the 
+    range_containing_table function from excel_ranges.
+If force is True and a table already exists with tbl_name, remove that table and replace it with the new one
+    '''
     wb = openpyxl.open(fname)
-    style = TableStyleInfo(name="TableStyleMedium9", 
-                           showFirstColumn=False,
-                           showLastColumn=False, 
-                           showRowStripes=True, 
-                           showColumnStripes=False)
-    tbl = Table(displayName = tbl_name, ref = rng)
-    tbl.tableStyleInfo = style
-    wb[sheet_name].add_table(tbl)
-    wb.save(fname)
-    wb.close()
+    if 'DataFrame' in str(type(rng)):
+        rng = range_containing_table(rng)
+    try:
+        style = TableStyleInfo(
+            name="TableStyleMedium9",
+            showFirstColumn=False,
+            showLastColumn=False,
+            showRowStripes=True,
+            showColumnStripes=False,
+        )
+        tbl = Table(displayName=tbl_name, ref=rng)
+        tbl.tableStyleInfo = style
+        if force and tbl_name in wb[sheet_name].tables:
+            del wb[sheet_name].tables[tbl_name]
+        wb[sheet_name].add_table(tbl)
+    finally:
+        wb.save(fname)
+        wb.close()
